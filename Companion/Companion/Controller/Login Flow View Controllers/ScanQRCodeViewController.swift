031c9eab6e70789028daf48b4706eda1a170f47d
Ektubaryan Narek <ektubaryannarek1993@gmail.com>
2018-02-20 18:31:32 +0400

    QR code reader implemented.

--- Companion/Companion/Controller/Login Flow View Controllers/ScanQRCodeViewController.swift
+++ Companion/Companion/Controller/Login Flow View Controllers/ScanQRCodeViewController.swift
@@ -14,15 +14,28 @@ class ScanQRCodeViewController: UIViewController, AVCaptureMetadataOutputObjects
     
     @IBOutlet weak var nextButtonView: UIView!
     @IBOutlet weak var troubleMessageView: UIView!
-
     @IBOutlet weak var myView: UIView!
+    @IBOutlet weak var messageLabel: UILabel!
     
+    var qrCodeFrameView: UIView?
     var session: AVCaptureSession?
     var device: AVCaptureDevice?
     var input: AVCaptureDeviceInput?
     var output: AVCaptureMetadataOutput?
     var prevLayer: AVCaptureVideoPreviewLayer?
     
+    let supportedCodeTypes = [AVMetadataObjectTypeQRCode,
+                                      AVMetadataObjectTypeCode39Code,
+                                      AVMetadataObjectTypeCode93Code,
+                                      AVMetadataObjectTypeCode128Code,
+                                      AVMetadataObjectTypeEAN8Code,
+                                      AVMetadataObjectTypeEAN13Code,
+                                      AVMetadataObjectTypeFace,
+                                      AVMetadataObjectTypePDF417Code,
+                                      AVMetadataObjectTypeITF14Code,
+                                      AVMetadataObjectTypeDataMatrixCode,
+                                      AVMetadataObjectTypeInterleaved2of5Code,
+                                      AVMetadataObjectTypeQRCode]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,7 +46,9 @@ class ScanQRCodeViewController: UIViewController, AVCaptureMetadataOutputObjects
         let troubleMessageGesture = UITapGestureRecognizer(target: self, action: #selector(troubleButtonClicked))
         self.nextButtonView.addGestureRecognizer(nextButtonGesture)
         self.troubleMessageView.addGestureRecognizer(troubleMessageGesture)
-        createSession ()
+        
+        // create capture session
+        createSession()
     }
     
     func makeNavBarTransparent(){
@@ -52,34 +67,48 @@ class ScanQRCodeViewController: UIViewController, AVCaptureMetadataOutputObjects
     }
     
     func createSession() {
-        session = AVCaptureSession()
         device = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
         
         guard let captureDevice = device else {
             print("Failed to get the camera device")
             return
         }
-        
         do {
+            session = AVCaptureSession()
             let input = try AVCaptureDeviceInput(device: captureDevice)
             session?.addInput(input)
+            let output = AVCaptureMetadataOutput()
+            session?.addOutput(output)
+            output.setMetadataObjectsDelegate(self, queue: .main)
+            output.metadataObjectTypes = supportedCodeTypes
+            
+            // Initialize QR Code Frame to highlight the QR code
+            qrCodeFrameView = UIView()
+            
+            if let qrCodeFrameView = qrCodeFrameView {
+                qrCodeFrameView.layer.borderColor = UIColor.green.cgColor
+                qrCodeFrameView.layer.borderWidth = 2
+                myView.addSubview(qrCodeFrameView)
+                myView.bringSubview(toFront: qrCodeFrameView)
+            }
         } catch {
             print(error)
             return
         }
         
         prevLayer = AVCaptureVideoPreviewLayer(session: session)
-        prevLayer?.frame.size = myView.frame.size
         prevLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
+        prevLayer?.frame.size = myView.frame.size
         myView.layer.addSublayer(prevLayer!)
+        
         session?.startRunning()
+        
     }
     
     func cameraWithPosition(_ position: AVCaptureDevicePosition) -> AVCaptureDevice? {
         if let deviceDescoverySession = AVCaptureDeviceDiscoverySession.init(deviceTypes: [AVCaptureDeviceType.builtInWideAngleCamera],
                                                                              mediaType: AVMediaTypeVideo,
                                                                              position: AVCaptureDevicePosition.unspecified) {
-            
             for device in deviceDescoverySession.devices {
                 if device.position == position {
                     return device
@@ -88,5 +117,48 @@ class ScanQRCodeViewController: UIViewController, AVCaptureMetadataOutputObjects
         }
         return nil
     }
+    
+    func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [Any]!, from connection: AVCaptureConnection!){
+        if metadataObjects.count == 0 {
+            qrCodeFrameView?.frame = CGRect.zero
+            messageLabel.text = "No QR code is detected"
+            return
+        }
+        
+        let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+        
+        if metadataObj.type == AVMetadataObjectTypeQRCode {
+            // If the found metadata is equal to the QR code metadata then update the status label's text and set the bounds
+            let barCodeObject = self.prevLayer?.transformedMetadataObject(for: metadataObj)
+            self.qrCodeFrameView?.frame = barCodeObject!.bounds
+            
+            if metadataObj.stringValue != nil {
+                self.messageLabel.text = metadataObj.stringValue
+            }
+        }
+    }
 
+    // SWIFT 4.
+//    func metadataOutput( output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+//
+//        if metadataObjects.count == 0 {
+//            qrCodeFrameView?.frame = CGRect.zero
+//            messageLabel.text = "No QR code is detected"
+//            return
+//        }
+//
+//        let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+//
+//        if metadataObj.type == AVMetadataObjectTypeQRCode {
+//            // If the found metadata is equal to the QR code metadata then update the status label's text and set the bounds
+//            let barCodeObject = self.prevLayer?.transformedMetadataObject(for: metadataObj)
+//            self.qrCodeFrameView?.frame = barCodeObject!.bounds
+//
+//            if metadataObj.stringValue != nil {
+//                self.messageLabel.text = metadataObj.stringValue
+//            }
+//        }
+//    }
 }
+
+
